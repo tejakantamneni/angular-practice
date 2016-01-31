489373479cb4c6ad29bad57409dfc9c16ef86db8
Teja Kantamneni <teja.kantamneni@gmail.com>
2016-01-31 14:09:20 -0500

    invoice v2 - added service

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -51,4 +51,20 @@ angular
       .otherwise({
         redirectTo: '/'
       });
+  })
+  .factory('currencyConverter', function() {
+    var currencies = ['USD', 'EUR', 'CNY'];
+    var usdToForeignRates = {
+      USD: 1,
+      EUR: 0.74,
+      CNY: 6.09
+    };
+    var convert = function (amount, inCurr, outCurr) {
+      return amount * usdToForeignRates[outCurr] / usdToForeignRates[inCurr];
+    };
+
+    return {
+      currencies: currencies,
+      convert: convert
+    };
   });
--- app/scripts/controllers/invoice.js
+++ app/scripts/controllers/invoice.js
@@ -8,23 +8,16 @@
  * Controller of the angularHelloApp
  */
 angular.module('angularHelloApp')
-  .controller('InvoiceCtrl', function () {
-    this.qty  = 1;
+  .controller('InvoiceCtrl', ['currencyConverter', function(currencyConverter) {
+    this.qty = 1;
     this.cost = 2;
-    this.inCurr = "USD";
-    this.currencies = ['EUR', 'USD', 'RUP'];
-    this.usdToForeignRates = {
-      USD: 1,
-      EUR: 0.74,
-      RUP: 68.0
-    };
-    this.total = function total(outCurr){
-      return this.convertCurrency(this.qty * this.cost, this.inCurr, outCurr);
-    };
-    this.convertCurrency = function convertCurrency(amount, inCurr, outCurr){
-      return amount * this.usdToForeignRates[outCurr] / this.usdToForeignRates[inCurr];
+    this.inCurr = 'EUR';
+    this.currencies = currencyConverter.currencies;
+
+    this.total = function total(outCurr) {
+      return currencyConverter.convert(this.qty * this.cost, this.inCurr, outCurr);
     };
     this.pay = function pay() {
       window.alert("Thanks!");
     };
-  });
+  }]);
